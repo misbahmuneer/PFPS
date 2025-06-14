f9b2f58d5132b362ce5ebe9da99c81e65373254e
misbahmuneer <mizbabhat8@gmail.com>
2025-06-14 23:57:24 +0530

    Create constan.c

---
+++ chapter1/constan.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    const float PI = 3.14159f;
+    float radius = 5.0f;
+
+    float area = PI * radius * radius;
+
+    printf("Area of the circle: %.2f\n", area);
+
+    return 0;
+}
