8a1e15de3045633c1947942940973481d6497fe1
misbahmuneer <mizbabhat8@gmail.com>
2025-06-19 23:20:01 +0530

    Create product-1-to-n.c

---
+++ chapter1/product-1-to-n.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    long long product = 1;
+    printf("Enter n: ");
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+        product *= i;
+    printf("Product = %lld\n", product);
+    return 0;
+}
